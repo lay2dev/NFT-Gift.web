4a3db41a72da6ec18f098c874216a4dc43327a6f
eclipes <751733381@qq.com>
2021-05-25 18:47:19 +0800

    API docking

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -7,34 +7,81 @@
         </div>
       </el-collapse-item>
       <el-collapse-item title="接收红包 - 组装交易" name="2">
+        <div class="tip" @click="getRedPacketData">
+          红包-接收红包：获取红包状态
+        </div>
         <div class="tip">
           红包-接收红包：通过收到的红包链接，组装交易，使用keyX进行签名，并将完整提交交易上链
         </div>
       </el-collapse-item>
       <el-collapse-item title="分享图片" name="3">
-        <div class="tip">红包-生成内含二维码的红包分享图片</div>
+        <div class="tip" @click="getShortData">
+          红包-生成内含二维码的红包分享图片
+        </div>
       </el-collapse-item>
     </el-collapse>
   </div>
 </template>
 <script>
+const HOST = 'http://devapi.unipass.me:3001'
+// const HOST = 'http://localhost:3001'
+Sea.Ajax.HOST = HOST
 export default {
   data() {
     return {
       activeName: '',
     }
   },
-  async created() {
-    // axios
-    Sea.Ajax.HOST = 'http://devapi.unipass.me:3001'
-    const res = await Sea.Ajax({
-      url: '/ntf/sdafs',
-      method: 'get',
-      data: {
-        address: 'dfadsf',
-      },
-    })
-    console.log('res', res)
+  methods: {
+    async getShortData() {
+      console.log('[api]')
+      const data = {
+        encrypt: 'todoencrypt',
+        authorization: 'authorization',
+        localKeySig: 'localKeySig',
+        localKeyPubkey: 'localKeyPubkey',
+        masterKeyPubkey: 'masterKeyPubkey',
+        localAuthInfo: 'localAuthInfo',
+        redPacket: [
+          {
+            keyPubkey: 'keyPubkey',
+            outpoints: 'outpoints',
+            outpointSize: 'outpointSize',
+          },
+        ],
+      }
+      console.log(data)
+      console.log('[Host]', Sea.Ajax.HOST)
+      const res = await Sea.Ajax({
+        url: '/ntf',
+        method: 'post',
+        data,
+      })
+      console.log('res', res)
+    },
+    async getRedPacketData() {
+      const key = 'todokey'
+      const data = {
+        password: 'todoencrypt',
+        address: 'authorization',
+      }
+      const res = await Sea.Ajax({
+        url:
+          '/ntf/' +
+          key +
+          '?password=' +
+          data.password +
+          '&address=' +
+          data.address,
+        method: 'get',
+      })
+      if (data.success) {
+        // todo get transfer data
+      } else {
+        // todo show no red packet
+      }
+      console.log(res)
+    },
   },
 }
 </script>

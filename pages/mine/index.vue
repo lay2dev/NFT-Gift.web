84ac9cb2d5746274c7e38be306035f07cd1f76fe
汪洋 <iwangyang@qq.com>
2021-06-10 14:04:23 +0800

    page-record

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -10,95 +10,90 @@
         @click="bindExit"
       />
     </header>
-    <pull-refresh :next="init">
-      <main>
-        <div class="nft-title">
-          <div class="title">NFT 资产</div>
-          <router-link to="/record">
-            <img class="icon" src="~/assets/img/ze-orders.svg" />
-          </router-link>
-        </div>
-        <div v-if="nftList.length === 0" v-loading="loading" class="not-found">
-          <img src="~/assets/img/not_found.svg" />
-          <div>你的资产宝箱里空空如也</div>
-        </div>
-        <el-collapse v-else v-model="activeList" class="nft-list">
-          <template v-for="(e, i) in nftList">
-            <el-collapse-item
-              :key="i"
-              :class="{ checked: e.checked.length > 0 }"
-            >
-              <template slot="title">
-                <div :key="i" class="nft">
-                  <div class="left" @click.stop="bindNFT(e)">
-                    <el-image
-                      class="nft-image"
-                      :src="e.renderer"
-                      alt="bg_image_url"
-                      fit="contain"
-                      lazy
-                    />
-                    <div class="info">
-                      <div class="name">
-                        {{ e.name }}
-                      </div>
-                      <div class="user">
-                        <el-image
-                          class="user-avator"
-                          :src="e.issuerAvatarUrl"
-                          alt="user-avator"
-                          fit="cover"
-                          lazy
-                        >
-                          <template #error>
-                            <div class="el-image__error" />
-                          </template>
-                        </el-image>
-                        <div class="user-name">
-                          {{ e.issuerName }}
-                        </div>
-                      </div>
+    <main>
+      <div class="nft-title">
+        <div class="title">NFT 资产</div>
+        <router-link to="/record">
+          <img class="icon" src="~/assets/img/ze-orders.svg" />
+        </router-link>
+      </div>
+      <div v-if="nftList.length === 0" v-loading="loading" class="not-found">
+        <img src="~/assets/img/not_found.svg" />
+        <div>你的资产宝箱里空空如也</div>
+      </div>
+      <el-collapse v-else v-model="activeList" class="nft-list">
+        <template v-for="(e, i) in nftList">
+          <el-collapse-item :key="i" :class="{ checked: e.checked.length > 0 }">
+            <template slot="title">
+              <div :key="i" class="nft">
+                <div class="left" @click.stop="bindNFT(e)">
+                  <el-image
+                    class="nft-image"
+                    :src="e.renderer"
+                    alt="bg_image_url"
+                    fit="contain"
+                    lazy
+                  />
+                  <div class="info">
+                    <div class="name">
+                      {{ e.name }}
                     </div>
-                  </div>
-                  <div class="right">
-                    <div class="total">
-                      {{ e.children && e.children.length }}
-                    </div>
-                    <div class="state" :class="e.txState">
-                      {{ stateDict[e.txState] }}
+                    <div class="user">
+                      <el-image
+                        class="user-avator"
+                        :src="e.issuerAvatarUrl"
+                        alt="user-avator"
+                        fit="cover"
+                        lazy
+                      >
+                        <template #error>
+                          <div class="el-image__error" />
+                        </template>
+                      </el-image>
+                      <div class="user-name">
+                        {{ e.issuerName }}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </template>
-              <div class="nft-all">
+                <div class="right">
+                  <div class="total">
+                    {{ e.children && e.children.length }}
+                  </div>
+                  <div class="state" :class="e.txState">
+                    {{ stateDict[e.txState] }}
+                  </div>
+                </div>
+              </div>
+            </template>
+            <div class="nft-all">
+              <el-checkbox
+                v-model="e.checkAll"
+                :indeterminate="e.isIndeterminate"
+                @change="bindCheckAll($event, i)"
+              >
+                全选
+              </el-checkbox>
+            </div>
+            <div class="nft-box">
+              <el-checkbox-group
+                v-model="e.checked"
+                @change="bindCheckChange($event, i)"
+              >
                 <el-checkbox
-                  v-model="e.checkAll"
-                  :indeterminate="e.isIndeterminate"
-                  @change="bindCheckAll($event, i)"
+                  v-for="nft in e.children"
+                  :key="nft.tokenId"
+                  class="nft-one"
+                  :label="nft.tokenId"
                 >
-                  全选
+                  #{{ nft.tokenId }}
                 </el-checkbox>
-              </div>
-              <div class="nft-box">
-                <el-checkbox-group
-                  v-model="e.checked"
-                  @change="bindCheckChange($event, i)"
-                >
-                  <el-checkbox
-                    v-for="nft in e.children"
-                    :key="nft.tokenId"
-                    class="nft-one"
-                    :label="nft.tokenId"
-                  >
-                    #{{ nft.tokenId }}
-                  </el-checkbox>
-                </el-checkbox-group>
-              </div>
-            </el-collapse-item>
-          </template>
-        </el-collapse>
-      </main>
-    </pull-refresh>
+              </el-checkbox-group>
+            </div>
+          </el-collapse-item>
+        </template>
+      </el-collapse>
+    </main>
     <transition name="el-zoom-in-bottom">
       <div v-show="showCheckBox" class="check-box">
         <el-button class="gift" @click="showGift = true">发红包</el-button>
@@ -112,13 +107,11 @@
 <script>
 import MineGift from '~/components/mine-gift.vue'
 import MineAsset from '~/components/mine-asset.vue'
-import PullRefresh from '~/components/pull-refresh.vue'
 
 export default {
   components: {
     MineAsset,
     MineGift,
-    PullRefresh,
   },
   data() {
     return {
@@ -285,185 +278,183 @@ export default {
     }
   }
 
-  .vue-pull-refresh {
-    >main {
-      .nft-title {
-        display: flex;
-        padding: 16px 22px 8px;
-        justify-content: space-between;
-        align-items: center;
+  >main {
+    .nft-title {
+      display: flex;
+      padding: 16px 22px 8px;
+      justify-content: space-between;
+      align-items: center;
 
-        .title {
-          font-size: 16px;
-        }
+      .title {
+        font-size: 16px;
+      }
 
-        .icon {
-          cursor: pointer;
-          height: 20px;
-          width: 20px;
+      .icon {
+        cursor: pointer;
+        height: 20px;
+        width: 20px;
+      }
+    }
+
+    .nft-list {
+      border: 0;
+      margin-bottom: 80px;
+
+      .checked {
+        .el-collapse-item__header {
+          border-bottom-color: var(--primary);
         }
       }
 
-      .nft-list {
+      .el-collapse-item__header {
+        height: 100%;
+        line-height: 100%;
+        cursor: auto;
+        border-bottom: 1px solid #F4F4F4;
+        padding-right: 12px;
+      }
+
+      .el-collapse-item__wrap {
         border: 0;
-        margin-bottom: 80px;
+      }
 
-        .checked {
-          .el-collapse-item__header {
-            border-bottom-color: var(--primary);
-          }
-        }
+      .el-collapse-item__content {
+        padding: 0;
+      }
 
-        .el-collapse-item__header {
-          height: 100%;
-          line-height: 100%;
-          cursor: auto;
-          border-bottom: 1px solid #F4F4F4;
-          padding-right: 12px;
-        }
+      .el-collapse-item__arrow {
+        color: #c5c5c5;
+        font-weight: bold;
+      }
 
-        .el-collapse-item__wrap {
-          border: 0;
-        }
+      .nft {
+        width: 100%;
+        padding: 16px 8px 16px 22px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
 
-        .el-collapse-item__content {
-          padding: 0;
-        }
-
-        .el-collapse-item__arrow {
-          color: #c5c5c5;
-          font-weight: bold;
-        }
-
-        .nft {
-          width: 100%;
-          padding: 16px 8px 16px 22px;
+        >.left {
+          cursor: pointer;
           display: flex;
           align-items: center;
-          justify-content: space-between;
+          margin-right: 6px;
 
-          >.left {
-            cursor: pointer;
-            display: flex;
-            align-items: center;
-            margin-right: 6px;
-
-            .nft-image {
-              background: #eee;
-              height: 50px;
-              width: 50px;
-              flex-shrink: 0;
-              box-shadow: 0px 1px 3px 1px rgba(0, 0, 0, 0.24);
-              border-radius: 4px;
-              overflow: hidden;
-            }
-
-            .info {
-              margin-left: 10px;
-
-              .name {
-                color: rgba(16, 16, 16, 100);
-                font-size: 16px;
-                line-height: 16px;
-              }
-
-              .user {
-                margin-top: 5px;
-                display: flex;
-                align-items: center;
-
-                .user-avator {
-                  width: 18px;
-                  height: 18px;
-                  border-radius: 2px;
-                  flex-shrink: 0;
-                }
-
-                .user-name {
-                  font-size: 14px;
-                  margin-left: 6px;
-                  color: #aaa;
-                }
-              }
-            }
-          }
-
-          .right {
-            display: flex;
-            align-items: center;
-
-            .total {
-              font-size: 18px;
-              font-weight: 500;
-            }
-
-            .state {
-              border-radius: 4px;
-              color: white;
-              padding: 2px 4px;
-              margin-left: 8px;
-            }
-
-            .state.committed {
-              padding: 0;
-            }
-
-            .state.pending {
-              background: var(--primary);
-            }
-
-            .state.submitting {
-              background: var(--success);
-            }
-          }
-        }
-
-        .nft-all {
-          padding: 8px 22px 0;
-          display: flex;
-          justify-content: flex-end;
-        }
-
-        .nft-box {
-          display: flex;
-          flex-wrap: wrap;
-          padding: 0 22px;
-
-          .nft-one {
-            margin-bottom: 14px;
-            border-radius: 5px;
-            background: #E6E6E6;
-            margin-right: 24px;
+          .nft-image {
+            background: #eee;
+            height: 50px;
             width: 50px;
-            height: 26px;
-            line-height: 26px;
-            text-align: center;
-
-            .el-checkbox__input {
-              display: none;
-            }
-
-            .el-checkbox__label {
-              padding-left: 0;
-            }
+            flex-shrink: 0;
+            box-shadow: 0px 1px 3px 1px rgba(0, 0, 0, 0.24);
+            border-radius: 4px;
+            overflow: hidden;
           }
 
-          .nft-one.is-checked {
-            background: var(--primary);
+          .info {
+            margin-left: 10px;
 
-            .el-checkbox__input.is-checked + .el-checkbox__label {
-              color: #FFF;
+            .name {
+              color: rgba(16, 16, 16, 100);
+              font-size: 16px;
+              line-height: 16px;
             }
+
+            .user {
+              margin-top: 5px;
+              display: flex;
+              align-items: center;
+
+              .user-avator {
+                width: 18px;
+                height: 18px;
+                border-radius: 2px;
+                flex-shrink: 0;
+              }
+
+              .user-name {
+                font-size: 14px;
+                margin-left: 6px;
+                color: #aaa;
+              }
+            }
+          }
+        }
+
+        .right {
+          display: flex;
+          align-items: center;
+
+          .total {
+            font-size: 18px;
+            font-weight: 500;
+          }
+
+          .state {
+            border-radius: 4px;
+            color: white;
+            padding: 2px 4px;
+            margin-left: 8px;
+          }
+
+          .state.committed {
+            padding: 0;
+          }
+
+          .state.pending {
+            background: var(--primary);
+          }
+
+          .state.submitting {
+            background: var(--success);
           }
         }
       }
 
-      .not-found {
-        text-align: center;
-        font-weight: 300;
-        font-size: 16px;
-        margin: 10px;
+      .nft-all {
+        padding: 8px 22px 0;
+        display: flex;
+        justify-content: flex-end;
       }
+
+      .nft-box {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 0 22px;
+
+        .nft-one {
+          margin-bottom: 14px;
+          border-radius: 5px;
+          background: #E6E6E6;
+          margin-right: 24px;
+          width: 50px;
+          height: 26px;
+          line-height: 26px;
+          text-align: center;
+
+          .el-checkbox__input {
+            display: none;
+          }
+
+          .el-checkbox__label {
+            padding-left: 0;
+          }
+        }
+
+        .nft-one.is-checked {
+          background: var(--primary);
+
+          .el-checkbox__input.is-checked + .el-checkbox__label {
+            color: #FFF;
+          }
+        }
+      }
+    }
+
+    .not-found {
+      text-align: center;
+      font-weight: 300;
+      font-size: 16px;
+      margin: 10px;
     }
   }
 
--- pages/record/index.vue
+++ pages/record/index.vue
@@ -1,64 +1,123 @@
 <template>
   <div id="page-record">
     <back />
-    <div class="title"></div>
-    <pull-refresh :next="init">
-      <record-nfts
-        :loading.sync="loading"
-        :nfts.sync="nftsCreate"
-        @cancel="bindCancel"
-      />
-      <div class="line"></div>
-      <record-nfts class="history" :nfts.sync="nftsOther" />
-    </pull-refresh>
+    <div class="page-title">红包记录</div>
+    <el-collapse v-model="activeList" class="records">
+      <el-collapse-item
+        v-for="(e, i) in records"
+        :key="i"
+        :name="i"
+        class="record"
+        :class="e.direction"
+      >
+        <div slot="title" class="record-title">
+          <template v-if="e.direction === 'in'">
+            <img src="~/assets/img/record-in.svg" alt="in" />
+            <div class="text">收到</div>
+            <div class="red">{{ e.nftNum }} NFT</div>
+          </template>
+          <template v-else>
+            <img src="~/assets/img/record-out.svg" alt="out" />
+            <div class="text">发出</div>
+            <div class="red">{{ e.packetNum }} 红包 {{ e.nftNum }} NFT</div>
+          </template>
+          <div class="date">
+            {{ dayjs(e.createdAt).format('M月D日 HH:mm') }}
+          </div>
+          <div class="state">{{ StatusDictBig[e.status] }}</div>
+        </div>
+        <div class="record-box">
+          <div class="left">
+            <div class="line">
+              <span>
+                红包状态：<span class="black">{{
+                  StatusDictBig[e.status]
+                }}</span>
+              </span>
+            </div>
+            <div class="line">
+              <span>
+                未被领取：<span class="red"
+                  >{{ e.packetNum - e.picked }} 个红包
+                </span>
+              </span>
+            </div>
+            <div class="line">
+              <span>
+                已被领取：<span>{{ e.picked }} 个红包</span>
+              </span>
+            </div>
+            <div class="line">
+              发起时间：<span>{{
+                dayjs(e.createdAt).format('YYYY年M月D日 HH:mm')
+              }}</span>
+            </div>
+          </div>
+          <div class="right">
+            <div class="btn">
+              <el-button
+                size="mini"
+                icon="el-icon-share"
+                @click="bindShare(nft)"
+              >
+                分享
+              </el-button>
+            </div>
+            <div class="btn">
+              <el-button
+                size="mini"
+                icon="el-icon-refresh-left"
+                @click="bindCancel(nft)"
+              >
+                撤回
+              </el-button>
+            </div>
+          </div>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
   </div>
 </template>
 
 <script>
-import RecordNfts from '~/components/record-nfts.vue'
-import PullRefresh from '~/components/pull-refresh.vue'
-
+import dayjs from 'dayjs'
 export default {
-  components: {
-    RecordNfts,
-    PullRefresh,
-  },
+  components: {},
   data() {
     return {
       loading: false,
       address: '',
-      nftsCreate: [],
-      nftsOther: [],
+      records: [],
+      activeList: [0, 1, 2],
+      StatusDictBig: {
+        create: '创建红包',
+        init: '领取中',
+        pending: '进行中',
+        committed: '领取完成',
+        cancel: '已撤销',
+        fail: '领取失败',
+      },
+      statusDictSmall: {
+        create: '创建红包',
+        init: '领取中',
+        pending: '确认中',
+        committed: '领取完成',
+        cancel: '已撤销',
+        fail: '领取失败',
+      },
     }
   },
   async mounted() {
     const provider = await Sea.bindLogin()
     if (provider) {
       this.address = provider._address.addressString
-      // this.init()
-      this.$message('正在开发')
+      this.init()
     } else {
       this.$router.replace('/')
     }
   },
-  sockets: {
-    connect() {
-      console.log('sockets', 'connect')
-      this.$socket.emit('login', { type: 'address', value: this.address })
-    },
-    newTx(data) {
-      console.log('sockets-newTx-')
-      // todo 刷新
-      console.log(data)
-    },
-    disconnect() {
-      console.log('sockets-disconnect：', '已经断开 socket 链接')
-    },
-    reconnect() {
-      this.$socket.emit('connect')
-    },
-  },
   methods: {
+    dayjs,
     async init() {
       this.loading = true
       const res = await Sea.Ajax({
@@ -66,15 +125,16 @@ export default {
         method: 'post',
         data: {
           address: this.address,
-          limit: 20,
+          limit: 1000,
           page: 0,
         },
       })
-      const filter = ['create', 'init', 'pending']
-      const arr = res.accept
-      this.nftsCreate = arr.filter((e) => filter.includes(e.txState))
-      this.nftsOther = arr.filter((e) => !filter.includes(e.txState))
+      console.log('🌊', JSON.parse(JSON.stringify(res[0])))
+      this.records = res
       this.loading = false
+    },
+    bindShare(nft) {
+      this.$router.push(`/share/${nft.shortkey}`)
     },
     async bindCancel(nft) {
       this.loading = true
@@ -94,25 +154,147 @@ export default {
       }
     },
   },
+  sockets: {
+    connect() {
+      console.log('sockets', 'connect')
+      this.$socket.emit('login', { type: 'address', value: this.address })
+    },
+    newTx(data) {
+      console.log('sockets-newTx-')
+      // todo 刷新
+      console.log(data)
+    },
+    disconnect() {
+      console.log('sockets-disconnect：', '已经断开 socket 链接')
+    },
+    reconnect() {
+      this.$socket.emit('connect')
+    },
+  },
 }
 </script>
 
 <style lang="stylus">
 #page-record {
-  > .title {
-    font-size: 18px;
-    display: flex;
-    justify-content: center;
-    height: 60px;
+  .page-title {
+    margin-top: 8px;
+    font-size: 16px;
+    text-align: center;
+    color: rgba(16, 16, 16, 100);
+    height: 35px;
+    line-height: 35px;
   }
 
-  > .line {
-    height: 1px;
-    background: var(--info);
-  }
+  .records {
+    margin-top: 31px;
+    margin-bottom: 31px;
+    border: 0;
 
-  .record-nfts.history {
-    padding-top: 16px;
+    .el-collapse-item__header {
+      background: transparent;
+      height: 40px;
+      line-height: 40px;
+      border: 0;
+      padding-left: 14px;
+    }
+
+    .el-collapse-item__wrap {
+      border: 0;
+      background: transparent;
+    }
+
+    .el-collapse-item__content {
+      padding: 0;
+    }
+
+    .el-collapse-item__arrow {
+      color: #000;
+      font-weight: bold;
+      font-size: 18px;
+    }
+
+    .record {
+      margin: 0 10px 10px;
+      border-radius: 5px;
+      color: rgba(16, 16, 16, 1);
+      font-size: 16px;
+      background-color: rgba(128, 128, 128, 0.09);
+
+      .record-title {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        color: #aaa;
+
+        img {
+          width: 16px;
+          height: 16px;
+        }
+
+        .text {
+          margin-left: 4px;
+          color: rgba(16, 16, 16, 100);
+          font-size: 16px;
+          flex-shrink: 0;
+        }
+
+        .red {
+          font-size: 14px;
+          background: #FF8577;
+          color: #fff;
+          padding: 1px 8px;
+          line-height: initial;
+          border-radius: 4px;
+          margin-left: 7px;
+          flex-shrink: 0;
+        }
+
+        .date {
+          margin-left: auto;
+          flex-shrink: 0;
+        }
+
+        .state {
+          // min-width: 54px;
+          margin-left: 6px;
+          margin-right: 6px;
+          color: #999;
+        }
+      }
+
+      .record-box {
+        margin-top: 8px;
+        min-height: 20px;
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        padding: 0 10px;
+        color: #AAA;
+
+        .left {
+          .line {
+            margin-bottom: 8px;
+
+            .red {
+              color: #FF8577;
+            }
+
+            .black {
+              color: #000;
+            }
+          }
+        }
+
+        .right {
+          display: flex;
+          flex-direction: column;
+
+          .btn {
+            margin-bottom: 4px;
+          }
+        }
+      }
+    }
   }
 }
 </style>

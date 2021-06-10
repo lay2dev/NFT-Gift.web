11d01533112195d1d773a4fcbc25aa2a69e62b35
汪洋 <iwangyang@qq.com>
2021-06-10 16:16:30 +0800

    socket url issue

--- plugins/socket.js
+++ plugins/socket.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
 Vue.use(
   new VueSocketIO({
-    debug: true,
+    debug: process.env.NODE_ENV === 'development',
     connection: process.env.SOCKET_URL,
     options: {
       transports: ['websocket'],

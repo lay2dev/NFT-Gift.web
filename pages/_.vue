2e847615ebe8781b3a3f4a6bc12f7146a47b2e7d
汪洋 <iwangyang@qq.com>
2021-08-01 15:50:41 +0800

    page error fix

--- pages/_.vue
+++ pages/_.vue
@@ -1,22 +1,37 @@
 <template>
-  <div id="page-error">Not found page</div>
+  <div id="page-error">{{ message }}</div>
 </template>
 
 <script>
 export default {
-  created() {
-    // fix route lang bug
-    const fullPath = this.$route.fullPath
-    for (const lang of ['/en', '/zh']) {
-      if (fullPath.startsWith(lang)) {
-        this.$router.replace(fullPath.slice(lang.length))
-      }
+  data() {
+    return {
+      message: '',
     }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      // fix route lang bug
+      const fullPath = this.$route.fullPath
+      for (const lang of ['/en', '/zh']) {
+        if (fullPath.startsWith(lang)) {
+          this.$router.replace(fullPath.slice(lang.length))
+          return
+        }
+      }
+      this.message = 'Not found page'
+    },
   },
 }
 </script>
 <style lang="stylus">
 #page-error {
+  display: flex;
+  align-items: center;
   justify-content: center;
+  height: 100vh;
 }
 </style>

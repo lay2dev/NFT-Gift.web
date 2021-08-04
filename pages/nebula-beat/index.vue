028e0b38eecbcad453e0f988c5d9c37d81212ee2
汪洋 <iwangyang@qq.com>
2021-08-04 16:19:12 +0800

    page-ticket.stylus

--- pages/nebula-beat/index.vue
+++ pages/nebula-beat/index.vue
@@ -1,22 +1,24 @@
 <template>
-  <div id="page-ticket-1">
-    <el-image
-      class="background"
-      fit="cover"
-      :src="require('~/assets/img/cyber/background.jpg')"
-    />
-    <div class="btn-box">
-      <div class="btn" type="primary" @click="bindGet">{{ btnText }}</div>
-    </div>
-    <Support />
+  <div id="page-ticket">
+    <main>
+      <el-image
+        class="background"
+        fit="cover"
+        :src="require('~/assets/img/cyber/background.jpg')"
+      />
+      <div class="bottom">
+        <div class="btn-box" @click="bindGet">
+          <div class="btn">{{ btnText }}</div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 <script>
+import '~/pages/ticket/ticket.stylus'
 import Ticket from '~/pages/ticket/ticket.js'
-import Support from '~/components/support.vue'
 
 export default {
-  components: [Support],
   mixins: [Ticket],
   computed: {
     btnText() {
@@ -50,59 +52,3 @@ export default {
   },
 }
 </script>
-<style lang="stylus">
-#page-ticket-1 {
-  margin: 0 auto;
-  max-width: 500px;
-  min-height: 100vh;
-  position: relative;
-  background: #EEE;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-
-  .background {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  .btn-box {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    position: fixed;
-    // 高度 812px 距底 160px
-    // 高度 667px 距底 90px
-    // 求斜率 (160 - 90) / (812 - 667) = 0.48275862069
-    // 667 * (160 - 90) / (812 - 667) - 90 = 232
-    // bottom = h * 0.48275862069 - 232
-    bottom: calc(100vh * (160 - 90) / (812 - 667) - 232px);
-    right: 0;
-    left: 0;
-    user-select: none;
-
-    .btn {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 20px;
-      border-radius: 8px;
-      padding: 12px 30px;
-      background: rgba(68 143 202, 1);
-      color: white;
-    }
-
-    .btn:hover {
-      background: rgba(68 143 202, 0.8);
-    }
-  }
-}
-</style>
